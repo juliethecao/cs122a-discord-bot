a41abca4e0cf83274351f7fb14cedfe7d1af5110
Ryan <leehahnwoong@gmail.com>
2023-10-04 20:15:29 -0700

    feat: move channel name to env var

--- bot/discord_bot_api.py
+++ bot/discord_bot_api.py
@@ -16,7 +16,7 @@ categories = ["general", "assignment", "lectures", "discussion-hours", "social"]
 subcategories = ["A1", "A2", "A3", "A4", "A5", "A6"]
 
 client = discord.Client(intents=intents)
-discord_channel_name = "ed-bot"
+discord_channel_name = os.getenv("CHANNEL_NAME")
 
 @client.event
 async def on_ready():
